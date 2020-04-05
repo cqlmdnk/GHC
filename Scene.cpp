0180dfffcbcb855e536faa197f23668aeee17bf3
cqlmdnk <berkan.tezcan@ceng.deu.edu.tr>
2020-04-06 01:54:03 +0300

    Added level designer mod

--- Scene.cpp
+++ Scene.cpp
@@ -1,7 +1,15 @@
 #include "Scene.h"
 
+
 // Bu class daha yarým içine scenede olmasý gereken herþey olucak
 // Düþmanlarda dahil.
+Scene::Scene(HDC hDC) {
+	tiles[0] = new Bitmap(hDC, TEXT("resources/tile1.bmp"));
+	tiles[1] = new Bitmap(hDC, TEXT("resources/tile2.bmp"));
+	tiles[2] = new Bitmap(hDC, TEXT("resources/tile2.bmp")); // tile tipleri buradan yüklenecek
+	memset(p_iPlatform, 0, sizeof(p_iPlatform));
+
+}
 
 
 void Scene::addBackground(Bitmap* img) {
@@ -15,9 +23,73 @@ void Scene::drawBackground(HDC hDc, int x) {
 	int sp = 1;// Burasý için class oluþturulabilir ama ben gerek duymadým. Background þeklinde attributeeleri bitmap ve speed olabilirdi ama onun yerine hýzý yavaþ olana göre ekleyip burda hýz 
 				//oluþturup çarptým.
 	for (Bitmap* i : background) {
-		i->Draw(hDc, (sp * x % 1920 + 960 + 1920) % 1920, 0, TRUE);
-		i->Draw(hDc, (sp * x % 1920 - 960) % 1920, 0, TRUE);
+		i->Draw(hDc, (sp * (-x)*10 % 1920 + 960 + 1920) % 1920, 0, TRUE); 
+		i->Draw(hDc, (sp * (-x)*10 % 1920 - 960) % 1920, 0, TRUE);
 		sp = sp + 1;
 	}
+	for (size_t i = 0; i < 55; i++)
+	{
+		for (size_t j = 0; j < 30; j++)
+		{
+			switch (p_iPlatform[i+x][j])
+			{
+			case 1:
+				tiles[0]-> Draw(hDc, i*35, j * 35, TRUE);
+				break;
+			case 2:
+				tiles[1]->Draw(hDc, i * 35, j * 35, TRUE);
+				break;
+			default:
+				break;
+			}
+			
+		}
+	}
 
+}
+
+void Scene::addTile(int x, int y, int type)
+{
+		p_iPlatform[x][y] = type;
+}
+
+void Scene::saveLevel(char* levelName)
+{
+	std::ofstream  afile;
+	afile.open(levelName);
+	for (size_t i = 0; i < 6000; i++)
+	{
+		for (size_t j = 0; j < 30; j++)
+		{
+			afile << std::to_string(p_iPlatform[i][j]);
+			if(j != 29) // end of line
+				afile << " ";
+		}
+		if(i != 5999) // end of file
+			afile << std::endl;
+	}
+	afile.close();
+}
+
+void Scene::loadLevel(char * levelName)
+{
+	std::ifstream file;
+	file.open(levelName);
+	std::string str;
+	int i = 0, j = 0;
+	while (std::getline(file, str)) {							//iterating each line in file
+
+		char *token;							//split by space
+		char* line = new char[str.length()];
+		line = strdup(str.c_str());
+		token = strtok(line, " ");
+		while (token != NULL) {									//iterating each element in line
+			p_iPlatform[i][j++] = std::stoi(token); 
+			token = strtok(NULL, " ");
+			
+		}
+		j = 0;
+		i++;
+	}
+	file.close();
 }
--- Scene.h
+++ Scene.h
@@ -1,8 +1,12 @@
 #pragma once
 #include<windows.h>
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "GameObject.h"
-#include "Sprite.h"
+#include "Character.h"
+
 
 // Bu class daha yarým içine scenede olmasý gereken herþey olucak
 // Düþmanlarda dahil.
@@ -10,10 +14,15 @@
 class Scene
 {
 public:
-	Scene() {};
+	int p_iPlatform[6000][30] = { 0 }; // 200 -> x ekseni // 14 -> y ekseni
+	Scene(HDC hDC);
 	void addBackground(Bitmap* img);
 	void addGameObject(GameObject* obj);
 	void drawBackground(HDC hDc, int x);
+	void addTile(int x, int y, int type);
+	void saveLevel(char* levelName);
+	void loadLevel(char* levelName);
+	Bitmap* tiles[3];
 
 private:
 	std::vector<Bitmap*> background; // resimlerin tutulduðu array
